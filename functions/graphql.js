6fd19f300aae6040831a5f4fb977178cc743191d
shadowdreamer <uzumaki@yeah.net>
2020-04-16 15:28:13 +0800

    little fix

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,15 +1,26 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
 
 const typeDefs = gql`
+  type Cnm {
+    time:Int
+    reason:String
+  }
   type Query {
     hello: String
+    cnm:Cnm
   }
 `
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
-      return `Hello from Netlify function. https://bit.ly/2UXh0fD`
+      return `6666${root}`
+    },
+    cnm:()=>{
+      return {
+        time:5,
+        reason:"sa le ni ma"
+      }
     }
   }
 }
